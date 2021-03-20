2ba2b02cb25ffab5f0795f59287ca6831ca7914c
Urip Subekti <urip.subekti13@gmail.com>
2021-03-20 15:15:02 +0700

    add assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,19 @@
+import back from './back.png';
+import arrowDown from './arrow-down.png';
+import arrowRight from './arrow-right.png';
+import cart from './cart.png';
+import plus from './plus.png';
+import star from './star.png';
+import startHalf from './star-half.png';
+
+const icons = {
+  back,
+  arrowDown,
+  arrowRight,
+  cart,
+  plus,
+  star,
+  startHalf
+}
+
+export default icons;
